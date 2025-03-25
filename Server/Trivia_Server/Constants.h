e9b0cd85b474e2dc4de6efebc66c903256bc9d6a
IdodiDuck <ido.redid@gmail.com>
2025-03-25 20:42:01 +0200

    chore: v1.0.1/Added Server IP as constant to file

--- Server/Trivia_Server/Constants.h
+++ Server/Trivia_Server/Constants.h
@@ -6,9 +6,13 @@
 // Network Constants
 // =============================
 
+const std::string SERVER_IP_ADDRESS = "127.0.0.1";
 const int SERVER_PORT = 8820;
+
 const std::string EXIT_SERVER = "EXIT";
 const int BUFFER_SIZE = 10;
+
+
 
 // =============================
 // Error Codes Constants
