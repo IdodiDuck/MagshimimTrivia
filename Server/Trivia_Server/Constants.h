0b9b263ab6ec34f5094b2a9d9d1666c77b5c872f
IdodiDuck <ido.redid@gmail.com>
2025-03-25 18:50:28 +0200

    feat: v1.0.1/Added constants file including all constants and required enums

---
+++ Server/Trivia_Server/Constants.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+// =============================
+// Network Constants
+// =============================
+
+const int SERVER_PORT = 8820;
+const std::string EXIT_SERVER = "EXIT";
+const int BUFFER_SIZE = 10;
+
+// =============================
+// Error Codes Constants
+// =============================
+
