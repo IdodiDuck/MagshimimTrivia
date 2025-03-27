76695ab35318e5693980ecb2c6955a7288d169bf
IdodiDuck <ido.redid@gmail.com>
2025-03-27 20:52:57 +0200

    feat: v1.0.1/Added Communicator class which handles client-server communications

---
+++ Server/Trivia_Server/Communicator.h
@@ -0,0 +1,37 @@
+#pragma once
+
+// Network Build Importations - 
+#include <WinSock2.h>
+
+// STD Libraries Importations - 
+#include <thread>
+#include <map>
+#include <memory>
+
+// Local Files Importations - 
+#include "IRequestHandler.h"
+
+class Communicator
+{
+
+public:
+
+	~Communicator();
+	void startHandleRequests();
+
+private:
+	// Attributes - 
+	SOCKET m_serverSocket;
+	std::map <SOCKET, std::shared_ptr<IRequestHandler>> m_clients;
+
+	// Private Methods - 
+	void bindAndListen();
+	void handleNewClient(SOCKET clientSocket);
+
+	// Support Methods -
+	void sendHelloToClient(SOCKET clientSocket);
+	void receiveHelloFromClient(SOCKET clientSocket);
+	void disconnectClient(SOCKET removedSocket);
+	bool doesClientExists(const SOCKET clientSocket);
+};
+
