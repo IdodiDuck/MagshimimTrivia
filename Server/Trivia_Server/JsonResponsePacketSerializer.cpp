ea56d0102d36bd7c7c78a4282e0a837241a53d96
IdodiDuck <ido.redid@gmail.com>
2025-03-29 22:56:16 +0300

    feat: v1.0.2/Added JSON Response Packet Serializer Class (Still needs to finish the methods)

---
+++ Server/Trivia_Server/JsonResponsePacketSerializer.cpp
@@ -0,0 +1,17 @@
+#include "JsonResponsePacketSerializer.h"
+#include <nlohmann/json.hpp>
+
+std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const ErrorResponse errorResponse)
+{
+    
+}
+
+std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const LoginResponse loginResponse)
+{
+    
+}
+
+std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const SignupResponse signUpResponse)
+{
+    
+}
---
+++ Server/Trivia_Server/JsonResponsePacketSerializer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+
+#include "Constants.h"
+
+class JsonResponsePacketSerializer
+{
+
+public:
+    static std::vector<unsigned char> serializeResponse(const ErrorResponse errorResponse);
+    static std::vector<unsigned char> serializeResponse(const LoginResponse loginResponse);
+    static std::vector<unsigned char> serializeResponse(const SignupResponse signUpResponse);
+};
