984b34587f47b95057548b2742a5a25937a705f2
IdodiDuck <ido.redid@gmail.com>
2025-03-29 22:55:18 +0300

    feat: v1.0.2/Added error, login and signup responses structures

--- Server/Trivia_Server/Constants.h
+++ Server/Trivia_Server/Constants.h
@@ -12,9 +12,28 @@ const int SERVER_PORT = 8820;
 const std::string EXIT_SERVER = "EXIT";
 const int BUFFER_SIZE = 10;
 
+// =============================
+// Response Structs
+// =============================
 
+typedef struct LoginResponse
+{
+    unsigned int status;
+
+} LoginResponse;
+
+typedef struct SignupResponse
+{
+    unsigned int status;
+
+} SignupResponse;
+
+typedef struct ErrorResponse
+{
+    std::string message;
+
+} ErrorResponse;
 
 // =============================
 // Error Codes Constants
 // =============================
-
