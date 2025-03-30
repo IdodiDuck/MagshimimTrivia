03494d9e01718c740f2ce9476ea4a7d9e0c8aa09
IdodiDuck <ido.redid@gmail.com>
2025-03-30 18:39:34 +0300

    feat: v1.0.2/Added Login Request Handlers Virtual Methods (Still needs to fill them)

--- Server/Trivia_Server/LoginRequestHandler.cpp
+++ Server/Trivia_Server/LoginRequestHandler.cpp
@@ -1,1 +1,19 @@
 #include "LoginRequestHandler.h"
+
+#include "Constants.h"
+
+LoginRequestHandler::~LoginRequestHandler()
+{
+    // TODO
+}
+
+bool LoginRequestHandler::isRequestRelevant(const RequestInfo& info)
+{
+    // TODO (Implement this method)
+    return true;
+}
+
+RequestResult LoginRequestHandler::handleRequest(RequestInfo& info)
+{
+    return RequestResult();
+}
