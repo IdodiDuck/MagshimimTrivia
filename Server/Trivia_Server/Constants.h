39054d8595ee0c2028ee79b716e2ac53a7458d94
IdodiDuck <ido.redid@gmail.com>
2025-03-30 17:39:26 +0300

    v1.0.2/Feat: Added RequestInfo and RequestResult Structures

--- Server/Trivia_Server/Constants.h
+++ Server/Trivia_Server/Constants.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <ctime>
 
 // =============================
 // Network Constants
@@ -10,7 +12,7 @@ const std::string SERVER_IP_ADDRESS = "127.0.0.1";
 const int SERVER_PORT = 8820;
 
 const std::string EXIT_SERVER = "EXIT";
-const int BUFFER_SIZE = 10;
+const int BUFFER_SIZE = 10; // Remove/Modify later
 
 // =============================
 // Response Structs
@@ -33,6 +35,25 @@ typedef struct ErrorResponse
     std::string message;
 
 } ErrorResponse;
+
+// =============================
+// Handlers Structs
+// =============================
+
+typedef struct RequestResult
+{
+    std::vector<unsigned char> response;
+    IRequestHandler* newHandler;
+
+} RequestResult;
+
+typedef struct RequestInfo
+{
+    int requestID;
+    std::time_t receivalTime;
+    std::vector<unsigned char> buffer;
+
+} RequestInfo;
 
 // =============================
 // Error Codes Constants
