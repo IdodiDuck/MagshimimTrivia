5d152b2fc78ddc6524dcff199e0f9579bfb1ae72
TomTorem <tomtorem@gmail.com>
2025-04-01 17:41:15 +0300

    feet: v1.0.2/added handleRequest

--- Server/Trivia_Server/LoginRequestHandler.cpp
+++ Server/Trivia_Server/LoginRequestHandler.cpp
@@ -1,5 +1,8 @@
 #include "LoginRequestHandler.h"
 
+#include "JsonRequestPacketDeserializer.h"
+#include "JsonResponsePacketSerializer.h"
+
 #include "Constants.h"
 
 LoginRequestHandler::~LoginRequestHandler()
@@ -14,5 +17,45 @@ bool LoginRequestHandler::isRequestRelevant(const RequestInfo& info)
 
 RequestResult LoginRequestHandler::handleRequest(RequestInfo& info)
 {
-    return RequestResult();
+    if (info.requestID == LOGIN_REQUEST_CODE)
+    {
+        auto loginRequest = JsonRequestPacketDeserializer::deserializeLoginRequest(info.buffer);
+        
+        // Valid login request
+        if (loginRequest.has_value())
+        {
+            LoginRequest request = loginRequest.value();
+            // Database Login Validation here (V1.0.3)
+            
+            LoginResponse response;
+            response.status = SUCCESS;
+            RequestResult result;
+            result.response = JsonResponsePacketSerializer::serializeResponse(response);
+            result.newHandler = nullptr; // Modify later and change to next given handler
+
+            return result;
+        }
+        // TODO in-case of failure
+    }
+
+    else if (info.requestID == SIGNUP_REQUEST_CODE)
+    {
+        auto singupRequest = JsonRequestPacketDeserializer::deserializeSignupRequest(info.buffer);
+
+        // Valid login request
+        if (singupRequest.has_value())
+        {
+            SignupRequest request = singupRequest.value();
+            // Database Login Validation here (V1.0.3)
+
+            SignupResponse response;
+            response.status = SUCCESS;
+            RequestResult result;
+            result.response = JsonResponsePacketSerializer::serializeResponse(response);
+            result.newHandler = nullptr; // Modify later and change to next given handler
+
+            return result;
+        }
+        // TODO in-case of failure
+    }
 }
