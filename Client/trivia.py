721371c3f28da2f732211f80e848033057b17ddf
IdodiDuck <ido.redid@gmail.com>
2025-04-01 10:59:54 +0300

    fix: v1.0.2/Fixed creation of requests by protocol, 1 byte for code, 4 bytes for length and encoded JSON for the rest

--- Client/trivia.py
+++ Client/trivia.py
@@ -1,4 +1,6 @@
 import socket
+import struct
+import json
 from time import sleep
 
 def chooseOperation():
@@ -12,12 +14,12 @@ def chooseOperation():
     user_choice = input("\nWelcome to Server Testing Script\nPlease choose Login/SignUp: ")
 
     # Validating user choice (Can be a login request or signup request)
-    while ((user_choice.lower() != LOGIN_REQUEST) and (user_choice.lower() != "signup")):
+    while (user_choice.lower() not in OPERATION_CODES.keys()):
         print("Error! Please choose a valid operation!\n")
-        user_choice = input("\nWelcome to Server Testing Script\nPlease choose Login/Sign-Up: ").lower()
+        user_choice = input("\nWelcome to Server Testing Script\nPlease choose Login/Sign-Up: ")
 
     # Returning the protocol opcode for the desired operation
-    return OPERATION_CODES[user_choice]
+    return OPERATION_CODES[user_choice.lower()]
 
 def sendRequest(request_code, client_socket):
     LOGIN_REQUEST_CODE = 100
@@ -30,7 +32,7 @@ def sendRequest(request_code, client_socket):
         return sendSignUpRequest(client_socket)
     
     # The request is either login request or signup request (validated user choice before!!)
-
+    
 def sendLoginRequest(client_socket):
     USER_NAME_FIELD = "username"
     PASSWORD_FIELD = "password"
@@ -41,12 +43,15 @@ def sendLoginRequest(client_socket):
     user_name = input("Enter username: ")
     password = input("Enter password: ")
 
-    # Constructing the JSON by the entered fields, then constructing the request by the protocol
-    login_json = f'{{"{USER_NAME_FIELD}":"{user_name}", "{PASSWORD_FIELD}":"{password}"}}'
-    request_string = str(LOGIN_REQUEST_CODE) + str(len(login_json)) + login_json
-    print(f"Sending to server: {request_string}\n")
+    login_data = {USER_NAME_FIELD: user_name, PASSWORD_FIELD: password}
+    login_json = json.dumps(login_data)
+    json_length = len(login_json)
 
-    client_socket.sendall(request_string.encode()) # Sending the login-request as sequence of bytes
+    # Packing the Login request by the protocol B -> 1 Byte of LoginRequestCode, I -> Integer (4 Bytes) and then the JSON encoded
+    login_request = struct.pack(f'!B I {json_length}s', LOGIN_REQUEST_CODE, json_length, login_json.encode())
+    
+    print(f"Sending to server: {login_request.decode()}\n")
+    client_socket.sendall(login_request)
     server_msg = client_socket.recv(BUFFER_SIZE)
 
     print(f"Returned server message: {server_msg}\n")
@@ -63,14 +68,17 @@ def sendSignUpRequest(client_socket):
     password = input("Enter password: ")
     email = input("Enter email: ")
 
-    # Constructing the JSON by the entered fields, then constructing the request by the protocol
-    signup_json = f'{{"{USER_NAME_FIELD}":"{user_name}", "{PASSWORD_FIELD}":"{password}", "{EMAIL_FIELD}":"{email}"}}'
-    request_string = str(SIGN_UP_REQUEST_CODE) + str(len(signup_json)) + signup_json
-    print(f"Sending to server: {request_string}\n")
+    signup_data = {USER_NAME_FIELD: user_name, PASSWORD_FIELD: password, EMAIL_FIELD: email}
+    signup_json = json.dumps(signup_data)
+    json_length = len(signup_json)
 
-    client_socket.sendall(request_string.encode()) # Sending the signup-request as sequence of bytes
+    # Packing the signup request by the protocol 
+    signup_request = struct.pack(f'!B I {json_length}s', SIGN_UP_REQUEST_CODE, json_length, signup_json.encode())
+
+    print(f"Sending to server: {signup_request.decode()}\n")
+    client_socket.sendall(signup_request)
+
     server_msg = client_socket.recv(BUFFER_SIZE)
-
     print(f"Returned server message: {server_msg}\n")
 
 def main():
