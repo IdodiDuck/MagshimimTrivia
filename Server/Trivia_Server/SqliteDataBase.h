27fa6c8e2a566b9e2314aad06f68c48689296496
IdodiDuck <ido.redid@gmail.com>
2025-04-09 11:04:30 +0300

    fix: v1.0.3/Moved support methods and attributes as private inside the class instead of global

--- Server/Trivia_Server/SqliteDataBase.h
+++ Server/Trivia_Server/SqliteDataBase.h
@@ -3,30 +3,25 @@
 #include "IDatabase.h"
 
 #include "sqlite3.h"
-#include <string>
-#include <io.h>
-#include <iostream>
 
-enum dbResult
+enum class DatabaseResult
 {
 	DATABASE_ERROR = -1,
+	DATABASE_SUCCESS,
 	USER_EXISTS,
 	USER_NOT_FOUND,
 	PASSWORD_MATCH,
 	PASSWORD_DISMATCH
 };
 
-const std::string TRIVIA_DB = "trivia.db";
-
-sqlite3* db;
-
-int executeQuery(const std::string& sql);
-void initializeTriviaDB();
-
 
 class SqliteDataBase : IDatabase
 {
+
 public:
+
+	SqliteDataBase();
+	virtual ~SqliteDataBase();
 
 	bool open() override;
 	bool close() override;
@@ -35,4 +30,12 @@ public:
 	int doesPasswordMatch(const std::string& user, const std::string& password) override;
 	int addNewUser(const std::string& user, const std::string& password, const std::string& email) override;
 
+private:
+	sqlite3* _dataBase;
+	std::string _dataBaseName;
+
+	// Support Methods - 
+	int executeQuery(const std::string& sql);
+	void initializeTriviaDB();
+	bool isDataBaseOpen();
 };
