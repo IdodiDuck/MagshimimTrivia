39ec1f276175c3f574918f001a4d185714ec975e
IdodiDuck <ido.redid@gmail.com>
2025-04-13 21:09:25 +0300

    refactor: v1.0.3/RequestHandlerFactory creates it's Login Manager and database attribute based on weak_ptr given database

--- Server/Trivia_Server/RequestHandlerFactory.cpp
+++ Server/Trivia_Server/RequestHandlerFactory.cpp
@@ -1,5 +1,13 @@
 #include "RequestHandlerFactory.h"
 
+#include "LoginRequestHandler.h"
+
+RequestHandlerFactory::RequestHandlerFactory(std::weak_ptr<IDatabase> database):
+	m_database(database), m_loginManager(database)
+{
+
+}
+
 std::unique_ptr<LoginRequestHandler> RequestHandlerFactory::createLoginRequestHandler()
 {
 	return std::make_unique<LoginRequestHandler>(*this);
--- Server/Trivia_Server/RequestHandlerFactory.h
+++ Server/Trivia_Server/RequestHandlerFactory.h
@@ -1,13 +1,16 @@
 #pragma once
 
 #include "LoginManager.h"
-#include "LoginRequestHandler.h"
 #include "MenuRequestHandler.h"
+
+class LoginRequestHandler;
 
 class RequestHandlerFactory
 {
 
 public:
+	RequestHandlerFactory(std::weak_ptr<IDatabase> database);
+
 	std::unique_ptr<LoginRequestHandler> createLoginRequestHandler();
 	LoginManager& getLoginManager();
 
