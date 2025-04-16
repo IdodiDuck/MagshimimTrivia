0ce67ea4dbb1de25ebea7377f144e72c2b8e8ae7
IdodiDuck <ido.redid@gmail.com>
2025-04-16 11:46:13 +0300

    feat: v2.0.0/Added Room Structures to be returned from deserializer for V2

--- Server/Trivia_Server/JsonRequestPacketDeserializer.h
+++ Server/Trivia_Server/JsonRequestPacketDeserializer.h
@@ -8,6 +8,7 @@
 // Request Structs
 // ===========================
 
+// Login and SignUp Structs (V1) - 
 typedef struct LoginRequest
 {
 	std::string username;
@@ -23,6 +24,27 @@ typedef struct SignupRequest
 
 } SignupRequest;
 
+// Room Structs (V2) - 
+typedef struct GetPlayersInRoomRequest
+{
+	unsigned int roomId;
+
+} GetPlayersInRoomRequest;
+
+typedef struct JoinRoomRequest
+{
+	unsigned int roomId;
+
+} JoinRoomRequest;
+
+typedef struct CreateRoomRequest
+{
+	std::string roomName;
+	unsigned int maxUsers;
+	unsigned int questionCount;
+	unsigned int answerTimeout;
+
+} CreateRoomRequest;
 
 
 class JsonRequestPacketDeserializer
