df78215bd5ac986e9e17ea4f258ac250033df616
TomTorem <tomtorem@gmail.com>
2025-05-03 18:27:54 +0300

    feat v2.0.1/Added Main Menu screen

---
+++ Client/TriviaClient/Views/MainMenu.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TriviaClient
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateRoomBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void JoinRoomBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StatisticsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void SignOutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
