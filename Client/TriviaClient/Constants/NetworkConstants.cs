86d9a0d5a5c54cf34104037d9eadc1025fc5e344
IdodiDuck <ido.redid@gmail.com>
2025-05-03 13:50:35 +0300

    build: v2.0.1/Rebased project, using directories to order all the files

---
+++ Client/TriviaClient/Constants/NetworkConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaClient.Constants
+{
+    public static class NetworkConstants
+    {
+        public const string SERVER_IP_ADDRESS = "127.0.0.1";
+        public const int SERVER_PORT = 8820;
+    }
+}
---
+++ Client/TriviaClient/Constants/StatusCodes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaClient.Constants
+{
+    public static class StatusCodes
+    {
+        public const int SUCCESS = 0;
+        public const int FAILURE = 1;
+    }
+}
