63c1ccf3d321fbec2f36119dadf18d1dd1939857
TomTorem <tomtorem@gmail.com>
2025-05-04 18:35:38 +0300

    feat v2.0.1/Added Game Lobby Screen

---
+++ Client/TriviaClient/Views/GameLobby.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TriviaClient
+{
+    /// <summary>
+    /// Interaction logic for GameLobby.xaml
+    /// </summary>
+    public partial class GameLobby : Window
+    {
+        public GameLobby()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            // Send Exit Message
+        }
+
+        private void ReadyCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartGameButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LeaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
