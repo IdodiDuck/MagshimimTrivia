3e1fd6d0edacae701d636c390a4f7bcd1c10d2f8
IdodiDuck <ido.redid@gmail.com>
2025-05-06 19:09:36 +0300

    refactor: v2.0.1/Made a generic deserializer as Tom did in order to develop usefully the Communicator

---
+++ Client/TriviaClient/Infrastructure/Deserializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.Json;
+
+namespace TriviaClient.Infrastructure
+{
+    internal class Deserializer
+    {
+        public static T? DeserializeResponse<T>(byte[] buffer)
+        {
+            const int HEADERS_BYTE_SIZE = 5;
+
+            string json = System.Text.Encoding.ASCII.GetString(buffer[HEADERS_BYTE_SIZE..]);
+
+            return JsonSerializer.Deserialize<T>(json);
+        }
+
+    }
+}
