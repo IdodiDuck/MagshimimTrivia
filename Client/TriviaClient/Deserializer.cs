668bbf25c196080d5dde9f0f025ab394702cacec
TomTorem <tomtorem@gmail.com>
2025-05-06 16:19:22 +0300

    feat: v2.0.1/Added Deserialization to client

---
+++ Client/TriviaClient/Deserializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using TriviaClient.Constants;
+
+namespace TriviaClient
+{
+    internal class Deserializer
+    {
+        public static T? DeserializeResponse<T>(byte[] data) where T : class
+        {
+            try
+            {
+                string json = System.Text.Encoding.UTF8.GetString(data);
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
