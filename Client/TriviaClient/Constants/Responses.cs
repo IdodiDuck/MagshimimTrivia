d9c9567ef358e2199833e0efd80eb5561d79e31b
IdodiDuck <ido.redid@gmail.com>
2025-05-09 13:08:22 +0300

    fix: v2.0.1/Fixed Responses Fields names to match the returned server responses fields for deserialization

--- Client/TriviaClient/Constants/Responses.cs
+++ Client/TriviaClient/Constants/Responses.cs
@@ -10,65 +10,65 @@ namespace TriviaClient.Constants
     {
         public class ErrorResponse
         {
-            public string Message { get; set; } = string.Empty;
+            public string message { get; set; } = string.Empty;
         }
 
         public class LoginResponse
         {
-            public uint Status { get; set; }
+            public uint status { get; set; }
         }
 
         public class SignupResponse
         {
-            public uint Status { get; set; }
+            public uint status { get; set; }
         }
 
         public class LogoutResponse
         {
-            public uint Status { get; set; }
+            public uint status { get; set; }
         }
 
         public class RoomData
         {
-            public uint Id { get; set; }
-            public string Name { get; set; } = string.Empty;
-            public uint MaxPlayers { get; set; }
-            public uint NumOfQuestionsInGame { get; set; }
-            public uint TimePerQuestion { get; set; }
-            public uint Status { get; set; }
+            public uint id { get; set; }
+            public string name { get; set; } = string.Empty;
+            public uint maxPlayers { get; set; }
+            public uint numOfQuestionsInGame { get; set; }
+            public uint timePerQuestion { get; set; }
+            public uint status { get; set; }
         }
 
         public class GetRoomsResponse
         {
-            public uint Status { get; set; }
-            public List<RoomData> Rooms { get; set; } = new List<RoomData>();
+            public uint status { get; set; }
+            public List<RoomData> rooms { get; set; } = new List<RoomData>();
         }
 
         public class GetPlayersInRoomResponse
         {
-            public List<string> Players { get; set; } = new List<string>();
+            public List<string> players { get; set; } = new List<string>();
         }
 
         public class JoinRoomResponse
         {
-            public uint Status { get; set; }
+            public uint status { get; set; }
         }
 
         public class CreateRoomResponse
         {
-            public uint Status { get; set; }
+            public uint status { get; set; }
         }
 
         public class GetHighScoreResponse
         {
-            public uint Status { get; set; }
-            public List<string> Statistics { get; set; } = new List<string>();
+            public uint status { get; set; }
+            public List<string> statistics { get; set; } = new List<string>();
         }
 
         public class GetPersonalStatsResponse
         {
-            public uint Status { get; set; }
-            public List<string> Statistics { get; set; } = new List<string>();
+            public uint status { get; set; }
+            public List<string> statistics { get; set; } = new List<string>();
         }
 
     }
