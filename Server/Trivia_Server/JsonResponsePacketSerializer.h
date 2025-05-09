887c17fd81d051b6e2fdd184b9777625c73c1ff5
IdodiDuck <ido.redid@gmail.com>
2025-05-09 23:10:01 +0300

    chore: v3.0.0/Added LeaveRoom Response Struct

--- Server/Trivia_Server/JsonResponsePacketSerializer.h
+++ Server/Trivia_Server/JsonResponsePacketSerializer.h
@@ -95,14 +95,22 @@ typedef struct GetRoomStateResponse
 
 } GetRoomStateResponse;
 
+typedef struct LeaveRoomResponse
+{
+    unsigned int status;
+
+} LeaveRoomResponse;
+
 class JsonResponsePacketSerializer
 {
 
 public:
+    // V1.0.0 Serializations - 
     static std::vector<unsigned char> serializeResponse(const ErrorResponse& errorResponse);
     static std::vector<unsigned char> serializeResponse(const LoginResponse& loginResponse);
     static std::vector<unsigned char> serializeResponse(const SignupResponse& signUpResponse);
 
+    // V2.0.0 Serializations - 
     static std::vector<unsigned char> serializeResponse(const LogoutResponse& loguotResponse);
     static std::vector<unsigned char> serializeResponse(const GetRoomsResponse& getRoomsResponse);
     static std::vector<unsigned char> serializeResponse(const GetPlayersInRoomResponse& getPlayersInRoomResponse);
@@ -110,4 +118,5 @@ public:
     static std::vector<unsigned char> serializeResponse(const CreateRoomResponse& createRoomResponse);
     static std::vector<unsigned char> serializeResponse(const getHighScoreResponse& getHighScoreResponse);
     static std::vector<unsigned char> serializeResponse(const getPersonalStatsResponse& getPersonalStatsResponse);
+
 };
