8de181245c7694399da4cb246a11886085ae1e2b
IdodiDuck <ido.redid@gmail.com>
2025-05-10 20:26:20 +0300

    feat: v3.0.0/Added implementation of Room Admin Request Handler

---
+++ Server/Trivia_Server/RoomAdminRequestHandler.cpp
@@ -0,0 +1,114 @@
+#include "RoomAdminRequestHandler.h"
+
+#include "RequestHandlerFactory.h"
+#include "JsonResponsePacketSerializer.h"
+
+RoomAdminRequestHandler::RoomAdminRequestHandler(std::weak_ptr<RequestHandlerFactory> handlerFactory, const LoggedUser& loggedUser, const Room& usedRoom):
+	m_handlerFactory(handlerFactory), m_user(loggedUser), m_roomManager(getFactorySafely()->getRoomManager()), m_room(usedRoom)
+{
+
+}
+
+bool RoomAdminRequestHandler::isRequestRelevant(const RequestInfo& info)
+{
+	return (info.requestID == static_cast<unsigned int>(RequestCode::CLOSE_ROOM_REQUEST)) ||
+		(info.requestID == static_cast<unsigned int>(RequestCode::START_GAME_REQUEST)) ||
+		(info.requestID == static_cast<unsigned int>(RequestCode::GET_ROOM_STATE_REQUEST)) ||
+		(info.requestID == static_cast<unsigned int>(RequestCode::LEAVE_ROOM_REQUEST));
+}
+
+RequestResult RoomAdminRequestHandler::handleRequest(const RequestInfo& requestInfo)
+{
+	switch (static_cast<RequestCode>(requestInfo.requestID))
+	{
+		case RequestCode::CLOSE_ROOM_REQUEST:
+			return closeRoom(requestInfo);
+
+		case RequestCode::START_GAME_REQUEST:
+			return startGame(requestInfo);
+
+		case RequestCode::GET_ROOM_STATE_REQUEST:
+			return getRoomState(requestInfo);
+
+		case RequestCode::LEAVE_ROOM_REQUEST:
+			return closeRoom(requestInfo);
+
+		default:
+			ErrorResponse errorResponse;
+			errorResponse.message = "Unknown request type.";
+			return
+			{
+				JsonResponsePacketSerializer::serializeResponse(errorResponse),
+				nullptr
+			};
+	}
+}
+
+RequestResult RoomAdminRequestHandler::closeRoom(const RequestInfo& info)
+{
+	this->m_roomManager.deleteRoom(m_room.getRoomData().id);
+
+	CloseRoomResponse closeRoomResponse = { };
+	closeRoomResponse.status = SUCCESS;
+
+	RequestResult response = { };
+	response.newHandler = this->getFactorySafely()->createMenuRequestHandler(m_user);
+	response.response = JsonResponsePacketSerializer::serializeResponse(closeRoomResponse);
+
+	return response;
+}
+
+RequestResult RoomAdminRequestHandler::startGame(const RequestInfo& info)
+{
+	this->m_room.changeRoomStatus(RoomStatus::GAME_STARTED);
+
+	StartGameResponse startGameResponse = { };
+	startGameResponse.status = SUCCESS;
+
+	RequestResult response = { };
+
+	// Create here GameRequestHandler instead!!! -> Temporary Handler
+	response.newHandler = getFactorySafely()->createRoomAdminRequestHandler(m_user, m_room.getRoomData().id);
+	response.response = JsonResponsePacketSerializer::serializeResponse(startGameResponse);
+
+	return response;
+}
+
+RequestResult RoomAdminRequestHandler::getRoomState(const RequestInfo& info)
+{
+	GetRoomStateResponse getRoomStateResponse;
+
+	RoomData usedRoomData = m_room.getRoomData();
+
+	getRoomStateResponse.status = SUCCESS;
+	getRoomStateResponse.hasGameBegun = (usedRoomData.status == RoomStatus::GAME_STARTED);
+	getRoomStateResponse.questionsCount = usedRoomData.numOfQuestionsInGame;
+	getRoomStateResponse.answerTimeout = usedRoomData.timePerQuestion;
+	getRoomStateResponse.players = std::vector<std::string>(m_room.getAllUsers().cbegin(), m_room.getAllUsers().cend());
+
+	RequestResult response;
+
+	if (getRoomStateResponse.hasGameBegun)
+	{
+		// Create here GameRequestHandler instead!!! -> Temporary Handler and initialize a new game!!!
+		response.newHandler = getFactorySafely()->createRoomAdminRequestHandler(m_user, m_room.getRoomData().id);
+	}
+
+	else
+	{
+		response.newHandler = getFactorySafely()->createRoomAdminRequestHandler(m_user, m_room.getRoomData().id);
+	}
+
+	response.response = JsonResponsePacketSerializer::serializeResponse(getRoomStateResponse);
+	return response;
+}
+
+std::shared_ptr<RequestHandlerFactory> RoomAdminRequestHandler::getFactorySafely()
+{
+	if (auto factory = m_handlerFactory.lock())
+	{
+		return factory;
+	}
+
+	throw std::runtime_error("RoomAdminRequestHandler: [ERROR]: RequestHandlerFactory is no longer available");
+}
---
+++ Server/Trivia_Server/RoomAdminRequestHandler.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "IRequestHandler.h"
+
+#include "RoomManager.h"
+#include "Room.h"
+
+class RequestHandlerFactory;
+
+class RoomAdminRequestHandler : public IRequestHandler
+{
+
+public:
+	// C'tor -
+	RoomAdminRequestHandler(std::weak_ptr<RequestHandlerFactory> handlerFactory, const LoggedUser& loggedUser, const Room& usedRoom);
+	
+	// Virtuals - 
+	bool isRequestRelevant(const RequestInfo& requestInfo) override;
+	RequestResult handleRequest(const RequestInfo& requestInfo) override;
+
+private:
+	// Attributes - 
+	Room m_room;
+	LoggedUser m_user;
+	RoomManager& m_roomManager;
+	std::weak_ptr<RequestHandlerFactory> m_handlerFactory;
+
+	// Private Methods - 
+	RequestResult closeRoom(const RequestInfo& info);
+	RequestResult startGame(const RequestInfo& info);
+	RequestResult getRoomState(const RequestInfo& info);
+
+	// Support Methods -
+	std::shared_ptr<RequestHandlerFactory> getFactorySafely();
+};
+
