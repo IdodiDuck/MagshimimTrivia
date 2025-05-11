48f9a4a8cd1b543beb74c2cf9fd6caf8f12fd870
IdodiDuck <ido.redid@gmail.com>
2025-05-11 11:50:03 +0300

    refactor: v3.0.0/Moved RoomData to Models instead of Responses file

---
+++ Client/TriviaClient/Models/RoomData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaClient.Models
+{
+    public class RoomData
+    {
+        public uint id { get; set; }
+        public string name { get; set; } = string.Empty;
+        public uint maxPlayers { get; set; }
+        public uint numOfQuestionsInGame { get; set; }
+        public uint timePerQuestion { get; set; }
+        public uint status { get; set; }
+    }
+}
