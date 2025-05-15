a0dc9a5234d333fcebeced0c584fb358ecf7c9ad
IdodiDuck <ido.redid@gmail.com>
2025-05-15 11:21:00 +0300

    feat: added handling of disconnection at Communicator (Needs fix?)

--- Server/Trivia_Server/Communicator.cpp
+++ Server/Trivia_Server/Communicator.cpp
@@ -1,16 +1,27 @@
 #include "Communicator.h"
 
 #include "Constants.h"
-#include "LoginRequestHandler.h"
 #include "JsonResponsePacketSerializer.h"
 #include "SocketHelper.h"
 #include "RequestHandlerFactory.h"
+
+#include "ClientConnectionException.h"
 
 #include <iostream>
 #include <ctime>
 
 Communicator::Communicator(std::weak_ptr<RequestHandlerFactory> handlerFactory): m_handlerFactory(handlerFactory), m_isRunning(true)
 {
+    const int SUCCESS = 0;
+
+    WSADATA wsaData;
+    int wsaInitResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    if (wsaInitResult != SUCCESS)
+    {
+        throw std::runtime_error("Error: WSAStartup failed.");
+    }
+
     this->m_serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     if (m_serverSocket == INVALID_SOCKET)
@@ -64,8 +75,6 @@ void Communicator::bindAndListen()
     serverAddr.sin_port = htons(SERVER_PORT);
     serverAddr.sin_addr.s_addr = INADDR_ANY;
 
-    m_serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-
     if (bind(m_serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
     {
         throw std::exception("Error: Failed to bind socket.\n");
@@ -81,24 +90,13 @@ void Communicator::bindAndListen()
 
 void Communicator::handleNewClient(SOCKET clientSocket)
 {
-    const std::string EMPTY_CONTENT = "";
-    const int EMPTY = 0;
-
     RequestInfo info;
 
     while (true)
     {
         try
         {
-            try
-            {
-                info = parseClientRequest(clientSocket);
-            }
-
-            catch (const std::exception& e)
-            {
-                continue;
-            }
+            info = parseClientRequest(clientSocket);
 
             if (m_clients[clientSocket]->isRequestRelevant(info))
             {
@@ -110,6 +108,13 @@ void Communicator::handleNewClient(SOCKET clientSocket)
                 std::cout << "Invalid Empty Request" << std::endl;
                 sendErrorResponse(clientSocket, "Error: Invalid type of request.");
             }
+        }
+
+        catch (const ClientConnectionException& e)
+        {
+            std::cerr << "Error handling client: " << e.what() << std::endl;
+            disconnectClient(clientSocket);
+            break;
         }
 
         catch (const std::exception& e)
@@ -127,8 +132,11 @@ void Communicator::disconnectClient(SOCKET removedSocket)
     {
         if (doesClientExists(removedSocket))
         {
+            m_clients[removedSocket]->handleDisconnection();
             m_clients.erase(removedSocket);
             closesocket(removedSocket);
+
+            std::cout << std::endl;
         }
     }
 
@@ -149,56 +157,45 @@ RequestInfo Communicator::parseClientRequest(const SOCKET clientSocket)
 {
     int requestCode = 0, requestLength = 0;
 
-    // Extracting the type of request (code) and the length of the JSON sent buffer by the protocol
-
     auto requestCodeOpt = SocketHelper::getRequestCode(clientSocket);
     if (!requestCodeOpt.has_value())  // If there's an error retrieving the request code
     {
-        throw std::runtime_error("Error: Failed to read request code from socket.");
+        throw ClientConnectionException("Error: Failed to read request code from socket.");
     }
 
     requestCode = requestCodeOpt.value();
 
-    // Get the request length
     auto requestLengthOpt = SocketHelper::getRequestLength(clientSocket);
-
     if (!requestLengthOpt.has_value())  // If there's an error retrieving the request length
     {
-        throw std::runtime_error("Error: Failed to read request length from socket.");
+        throw ClientConnectionException("Error: Failed to read request length from socket.");
     }
 
     requestLength = requestLengthOpt.value();
 
-    // Get the actual JSON data
     auto requestJSONDataOpt = SocketHelper::getData(clientSocket, requestLength);
     if (!requestJSONDataOpt.has_value())  // If there's an error receiving the JSON data
     {
-        throw std::runtime_error("Error: Failed to read JSON data from socket.");
+        throw ClientConnectionException("Error: Failed to read JSON data from socket.");
     }
 
     std::vector<unsigned char> requestJSONData = requestJSONDataOpt.value();
 
-    // Return the parsed request info
     return { requestCode, std::time(nullptr), requestJSONData };
 }
 
 void Communicator::processClientRequest(SOCKET clientSocket, RequestInfo& info)
 {
-    const std::string EMPTY_CONTENT = "";
-
     std::cout << "Handling the request, getting its results..." << std::endl;
-    RequestResult res = m_clients[clientSocket]->handleRequest(info);
+    RequestResult handlingResult = m_clients[clientSocket]->handleRequest(info);
 
     std::cout << "Giving the new handler to the client..." << std::endl;
-
     std::cout << "Old handler type: " << typeid(*m_clients[clientSocket]).name() << std::endl;
-
-    m_clients[clientSocket] = std::move(res.newHandler);
-
+    m_clients[clientSocket] = std::move(handlingResult.newHandler);
     std::cout << "New handler type: " << typeid(*m_clients[clientSocket]).name() << std::endl;
 
     std::cout << "Constructing response to be sent..." << std::endl;
-    std::vector<unsigned char> buffer = res.response;
+    std::vector<unsigned char> buffer = handlingResult.response;
     std::string response = (buffer.empty() ? "" : std::string(buffer.begin(), buffer.end()));
 
     sendClientResponse(clientSocket, buffer);
