e342a7ed5abdd04863f251fc8020d2d35dd26e3c
IdodiDuck <ido.redid@gmail.com>
2025-05-16 17:04:37 +0300

    feat: v3.0.0/Added Model of RoomInfo for comfortable informative representation of rooms in the list

---
+++ Client/TriviaClient/Models/RoomInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TriviaClient.Constants;
+
+
+namespace TriviaClient.Models
+{
+    public class RoomInfo
+    {
+        // Attributes - 
+        public string RoomName { get; set; }
+        public string AdminName { get; set; }
+        public uint NumberOfMembers { get; set; }
+        public uint roomId { get; set; }
+
+        // C'tor - 
+        public RoomInfo(string roomName, string adminName, uint numberOfPeople, uint roomid, RoomStatus roomStatus)
+        {
+            if (roomStatus == RoomStatus.GAME_STARTED)
+            {
+                this.RoomName = roomName + ", Game Started!";
+            }
+
+            else
+            {
+                this.RoomName = roomName;
+            }
+
+            AdminName = adminName;
+            NumberOfMembers = numberOfPeople;
+            this.roomId = roomid;
+        }
+
+        // Object String Representation - 
+        public override string ToString()
+        {
+            return this.RoomName + ", Admin: " + this.AdminName + ", Current in Room: " + this.NumberOfMembers;
+        }
+    }
+}
