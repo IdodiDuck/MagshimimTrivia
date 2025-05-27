2de146b49fa1cc7b9d6de14997e2156742c3695f
TomTorem <tomtorem@gmail.com>
2025-05-27 22:32:11 +0300

    feat: v4.0.0/added getQuestionForUser function for Game

--- Server/Trivia_Server/Game.cpp
+++ Server/Trivia_Server/Game.cpp
@@ -9,3 +9,23 @@ Game::Game(int gameId, std::vector<Question> questions, std::map<std::string, Ga
     }
 }
 
+Question Game::getQuestionForUser(const std::string& user)
+{
+    std::shared_lock lock(m_userMutex);
+
+    if (m_players.find(user) == m_players.end())
+    {
+        throw std::runtime_error("User not found in game");
+    }
+
+    GameData& data = m_players[user];
+    unsigned int currentIndex = data.correctAnswerCount + data.wrongAnswerCount;
+
+    if (currentIndex >= m_questions.size()) 
+    {
+        throw std::runtime_error("No more questions available for this user");
+    }
+
+    data.currentQuestion = m_questions[currentIndex];
+    return data.currentQuestion;
+}
