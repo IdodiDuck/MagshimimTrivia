8bad395e2ce8e4e2e5a6807e63f299fa1d256dc6
IdodiDuck <ido.redid@gmail.com>
2025-06-01 13:34:51 +0300

    feat: v4.0.0/Added Game-State Enum class to Client

---
+++ Client/TriviaClient/Constants/GameState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaClient.Constants
+{
+    public enum GameState : uint
+    {
+        WAITING_TO_START = 0,
+        STARTED,
+        WAITING_FOR_NEXT_QUESTION,
+        WAITING_FOR_ANSWER,
+        FINISHED
+    }
+}
