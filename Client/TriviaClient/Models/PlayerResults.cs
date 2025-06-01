a75a109b8f1a1ad33a54244dc4776b5bfb0b2106
IdodiDuck <ido.redid@gmail.com>
2025-06-01 13:32:55 +0300

    feat: v4.0.0/Added Player Results model class to Client

---
+++ Client/TriviaClient/Models/PlayerResults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaClient.Models
+{
+    public class PlayerResults
+    {
+        public string username { get; set; } = string.Empty;
+        public uint correctAnswerCount { get; set; }
+        public uint wrongAnswerCount { get; set; }
+        public uint averageAnswerTime { get; set; }
+    }
+}
