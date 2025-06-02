14927e143f02eca55740e6139d5068745d0891fd
IdodiDuck <ido.redid@gmail.com>
2025-06-02 17:08:53 +0300

    refactor: v4.0.0/Added Player Score to Model directory

---
+++ Client/TriviaClient/Models/PlayerScore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaClient.Models
+{
+    public class PlayerScore
+    {
+        public string Name { get; set; } = string.Empty;
+        public int CorrectAnswers { get; set; }
+        public string AvgTime { get; set; } = string.Empty;
+    }
+}
